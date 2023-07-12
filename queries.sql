3f68d61b2b123a87cdee0083fa661057de4898a3
jemba narcisse <cabliveer@gmail.com>
2023-07-12 21:12:24 +0200

    Update queries sql file

--- queries.sql
+++ queries.sql
@@ -18,6 +18,41 @@ SELECT * from animals WHERE name <> 'Gabumon';
 
 SELECT * from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
 
+Begin;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+Begin;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+commit;
+
+Begin;
+DELETE FROM animals; 
+
+Begin;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01'; 
+savepoint sp1;
+
+UPDATE animals SET weight_kg = -1*weight_kg 
+Rollback to sp1;
+
+UPDATE animals SET weight_kg = -1*weight_kg WHERE weight_kg < 0
+commit;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) as sum_escape FROM animals GROUP BY neutered ORDER BY sum_escape DESC;
+
+SELECT neutered, MIN(weight_kg) as min, MAX(weight_kg) as max FROM animals GROUP BY neutered;
+
+SELECT neutered, AVG(escape_attempts) FROM animals where date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY neutered;
+
 
 
 
