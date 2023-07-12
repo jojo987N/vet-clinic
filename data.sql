6d3326b1fcbf708f8da6fc0dc1c7b2f0a8063093
jemba narcisse <cabliveer@gmail.com>
2023-07-12 21:13:36 +0200

    Update data sql file

--- data.sql
+++ data.sql
@@ -12,3 +12,23 @@ VALUES ('Pikachu', '2021-01-07', 15.04, 1, false);
 INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
 VALUES ('Devimon', '2017-12-05', 11, 5, true);
 
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Charmander', '2020-02-08', -11, 0, false);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Plantmon', '2021-11-15', -5.7, 2, true);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Squirtle', '1993-04-02', -12.13, 3, false);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Angemon', '2005-06-12', -45, 1, true);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Boarmon', '2005-06-07', 20.4, 7, true);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Blossom', '1998-10-13', 17, 3, true);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+VALUES ('Ditto', '2022-05-14', 22, 4, true);
