aead6f9e7369e78b1f6452f56a1d9d3c92bb9735
jemba narcisse <cabliveer@gmail.com>
2023-07-13 16:32:26 +0200

    Update schema sql file

--- schema.sql
+++ schema.sql
@@ -10,3 +10,28 @@ CREATE TABLE animals (
 
 ALTER TABLE animals ADD species VARCHAR; 
 
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY, 
+    full_name VARCHAR,
+    age NUMERIC
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id); 
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INTEGER,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+
+
